9fb83bbcc03525dc3f94fd58ab9d5f7e92c2cfdb
Iranloye Hannah <iranloye8.hannah@gmail.com>
2025-06-14 19:18:34 +0100

    Update Vercel and Vite configuration for proper routing

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "",
+  base: '/',
   plugins: [
     tailwindcss(),
     react(),
   ],
+  server: {
+    port: 3000,
+    open: true
+  }
 })
